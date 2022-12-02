58aae3fa7020b487fc72a6822195cadf110b2331
constint <cm.mailing@mailoo.org>
2022-12-02 10:12:06 +0100

    Day 2

---
+++ src/day2.clj
@@ -0,0 +1,166 @@
+(ns day2)
+
+;--- Day 2: Rock Paper Scissors ---
+;
+;The Elves begin to set up camp on the beach. To decide whose tent gets to be closest to the snack storage, a giant Rock Paper Scissors tournament is already in progress.
+;
+;Rock Paper Scissors is a game between two players. Each game contains many rounds; in each round, the players each simultaneously choose one of Rock, Paper, or Scissors using a hand shape. Then, a winner for that round is selected: Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock. If both players choose the same shape, the round instead ends in a draw.
+(defn round-outcome? [moves]
+  (condp = moves
+    [:scissors :rock] :win
+    [:rock :rock] :draw
+    [:rock :paper] :win
+    [:paper :paper] :draw
+    [:paper :scissors] :win
+    [:scissors :scissors] :draw
+    :loss
+    ))
+;
+;Appreciative of your help yesterday, one Elf gives you an encrypted strategy guide (your puzzle input) that they say will be sure to help you win. "The first column is what your opponent is going to play: A for Rock, B for Paper, and C for Scissors. The second column--" Suddenly, the Elf is called away to help with someone's tent.
+;
+;The second column, you reason, must be what you should play in response: X for Rock, Y for Paper, and Z for Scissors. Winning every time would be suspicious, so the responses must have been carefully chosen.
+
+(def key->move
+  {"X" :rock
+   "A" :rock
+   "Y" :paper
+   "B" :paper
+   "Z" :scissors
+   "C" :scissors
+   })
+
+;
+;The winner of the whole tournament is the player with the highest score. Your total score is the sum of your scores for each round. The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
+
+(def move->score
+  {:rock     1
+   :paper    2
+   :scissors 3})
+
+(def outcome->score
+  {:loss 0
+   :draw 3
+   :win  6})
+;
+;Since you can't be sure if the Elf is trying to help you or trick you, you should calculate the score you would get if you were to follow the strategy guide.
+;
+;For example, suppose you were given the following strategy guide:
+;```
+;A Y
+;B X
+;C Z
+;```
+;
+(def example "A Y\nB X\nC Z")
+
+;This strategy guide predicts and recommends the following:
+;
+;- In the first round, your opponent will choose Rock (A), and you should choose Paper (Y). This ends in a win for you with a score of 8 (2 because you chose Paper + 6 because you won).
+;- In the second round, your opponent will choose Paper (B), and you should choose Rock (X). This ends in a loss for you with a score of 1 (1 + 0).
+;- The third round is a draw with both players choosing Scissors, giving you a score of 3 + 3 = 6.
+
+
+(defn parse-move [move-str]
+  (let [keys (clojure.string/split move-str #" ")]
+    (mapv key->move keys)))
+
+(parse-move "X B")
+
+(defn parse-rounds [input]
+  (->> input
+       (clojure.string/split-lines)
+       (map parse-move)
+       ))
+
+(def rounds (parse-rounds example))
+
+(first rounds)
+(round-outcome? (first rounds))
+
+
+(defn score-round [round]
+  (let [outcome (round-outcome? round)
+        my-move (second round)]
+    (+ (outcome->score outcome)
+       (move->score my-move))))
+
+(score-round (first rounds))
+
+(defn total-score [input]
+  (->> input
+       parse-rounds
+       (map score-round)
+       (reduce +)))
+
+
+;
+;In this example, if you were to follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
+
+(total-score example)
+
+;What would your total score be if everything goes exactly according to your strategy guide?
+
+(total-score (slurp "resources/day2.txt"))
+
+
+; --- Part Two ---
+;
+;The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+;
+(def key->move-or-outcome
+  {"X" :loss
+   "A" :rock
+   "Y" :draw
+   "B" :paper
+   "Z" :win
+   "C" :scissors
+   })
+;The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+;
+;-    In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+;-    In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+;-    In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+
+(defn round-move? [round]
+  (condp = round
+    [:scissors :loss] :paper
+    [:scissors :win] :rock
+    [:rock :win] :paper
+    [:rock :loss] :scissors
+    [:paper :win] :scissors
+    [:paper :loss] :rock
+    (first round)                                           ;; need a draw, play the same move
+    ))
+
+(defn parse-move-bis [move-str]
+  (let [keys (clojure.string/split move-str #" ")]
+    (mapv key->move-or-outcome keys)))
+
+(defn parse-rounds-bis [input]
+  (->> input
+       (clojure.string/split-lines)
+       (map parse-move-bis)
+       ))
+
+(defn score-round-bis [round]
+  (let [move (round-move? round)
+        outcome (second round)]
+    (+ (outcome->score outcome)
+       (move->score move))))
+
+(def rounds-bis (parse-rounds-bis example))
+
+(round-move? (first rounds-bis))
+;Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+(defn total-score-bis [input]
+  (->> input
+       parse-rounds-bis
+       (map score-round-bis)
+       (reduce +)))
+
+(total-score-bis example)
+;
+;Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+
+(total-score-bis (slurp "resources/day2.txt"))
